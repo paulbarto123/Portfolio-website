b67b953b767f81983166152d1371d83dda6d6f49
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-07-28 17:07:04 +0700

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
+// REACT FONT AWESOME
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+    
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+
         <a class="navbar-brand" href="#"><span className="logo">PAULUS SIAHAAN</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={faBars}
+              style={{ color:"white"}}
+          />
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -30,6 +38,7 @@ const Navbar = () => {
             </li>
             
           </ul>
+        </div>
         </div>
       </nav>
     )
